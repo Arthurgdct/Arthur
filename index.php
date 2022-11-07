2d9652151867b8728ca576a01f3a02cb5d94497c
Arthurgdct <grandclementarthur@gmail.com>
2022-11-07 04:11:46 +0100

    Création des fichiers et de la barre de navigation.

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Emblema+One&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/72e8ccb00a.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <div id="namecontainer">
+            <p id="firstname" class="txt-y">A<br>R<br>T<br>H<br>U<br>R</p>
+            <p id="lastname" class="txt-b bg-y">G<br>R<br>A<br>N<br>D<br>-<br>C<br>L<br>E<br>M<br>E<br>N<br>T</p>
+        </div>
+        <nav>
+            <div id="btncontainer">
+                <a href="#" class="btn-nav">Accueil</a>
+                <a href="#" class="btn-nav">Présentation</a>
+                <a href="#" class="btn-nav">Réalisations</a>
+                <a href="#" class="btn-nav">Compétences</a>
+                <a href="#" class="btn-nav">Mon Cv</a>
+                <a href="#" class="btn-nav">Contact</a>
+            </div>
+            <div id="iconcontainer">
+                <a href="https://www.linkedin.com/in/arthur-gdct/"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/Arthurgdct/"><i class="fa-brands fa-github"></i></a>
+            </div>
+        </nav>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>
